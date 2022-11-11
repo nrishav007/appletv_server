cf174a51352d241935865a6e4d917e3e48025866
Rishav Neogi <nrishav31@gmail.com>
2022-11-12 04:55:21 +0530

    code modified

---
+++ corsResolver.js
@@ -0,0 +1,16 @@
+function corsResolver(req, res, next) {
+    // Website you wish to allow to connect
+    // running front-end application on port 3000
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+}
+
+module.exports = corsResolver;
