28d925e9908071f6c80db4b6ef492306febe193b
Rishav Neogi <nrishav31@gmail.com>
2022-11-12 20:11:49 +0530

    code modified

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ var app = express();
 // const cores=require("./corsResolver")
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended:false}))
 // Create GET request
 app.get("/", (req, res) => {
     res.send(help);
